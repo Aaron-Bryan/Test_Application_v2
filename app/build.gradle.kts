74a667a02828f81bef96b0eae7607b17df57e008
Aaron-Bryan <aaronbryanparrilla@gmail.com>
2024-02-06 14:08:00 +0800

    Modifications

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 
 dependencies {
 
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
